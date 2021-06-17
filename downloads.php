659baad252f1a5e835ac21d8debd2b9218813e8e
Niklas Schachl <niklas.schachl@htl-wels.at>
2021-06-17 15:48:18 +0200

    Implemented Download page

--- downloads.php
+++ downloads.php
@@ -26,6 +26,23 @@
                 </ul>
             </nav>
         </header>
+        <h2>Downloads</h2>
+        <div>
+            <h3>Wallpaper</h3>
+            Hintergründe für deinen Desktop!<br>
+            Einfach anklicken zum herunterladen!<br>
+            <a href="images/WALLPAPER_Almhütte.png" download><img src="images/WALLPAPER_Almhütte.png" height="224" width="300"></a>
+            <a href="images/WALLPAPER_Bergpanorama.png" download><img src="images/WALLPAPER_Bergpanorama.png" height="224" width="300"></a>
+            <a href="images/WALLPAPER_Bergpanorama_Tal.png" download><img src="images/WALLPAPER_Bergpanorama_Tal.png" height="224" width="300"></a>
+            <a href="images/WALLPAPER_Bergpanorama_Weg.png" download><img src="images/WALLPAPER_Bergpanorama_Weg.png" height="224" width="300"></a>
+            <a href="images/WALLPAPER_Wald.png" download><img src="images/WALLPAPER_Wald.png" height="224" width="300"></a>
+        </div>
+        <div>
+            <h3>Informationsmaterial</h3>
+            Informieren sie sich über unsere Leistungen und Preise!<br>
+            <a href="pdf/nLearn-Leistungen-2021.pdf" download>Unsere Leistungen 2021</a><br>
+            <a href="pdf/nLearn-Preise-2021.pdf" download>Unsere Preise 2021</a><br>
+        </div>
         <footer>
             Copyright (C) 2021 nLearn<br>
             Copyright (C) 2021 Niklas Schachl<br>
